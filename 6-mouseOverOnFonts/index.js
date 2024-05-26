84570791990c49a19d9a15be17ee60a1a248bdbc
Rajesh kumar <144345870+rajeshkumar1406@users.noreply.github.com>
2024-05-26 14:07:32 +0530

    Update index.js

--- 6-mouseOverOnFonts/index.js
+++ 6-mouseOverOnFonts/index.js
@@ -1,24 +1,4 @@
-//for single element
 
-// var elem1=document.querySelector("#elem1")
-// var elemImage = document.querySelector("#elem1 img")
-
-// elem1.addEventListener("mousemove", function(event){
-//     elemImage.style.left=event.x+ "px"
-//     elemImage.style.top=event.y+ "px"
-// })
-
-// elem1.addEventListener("mouseenter", function(event){
-//     elemImage.style.opacity=1;
-// })
-
-
-// elem1.addEventListener("mouseleave", function(event){
-//     elemImage.style.opacity=0;
-// })
-
-
-// now for all elements
 var elem = document.querySelectorAll(".elem");
 
 elem.forEach(function(event){
